98bff52400da147f76abd3c7f7c6116db7354973
iliya68rus <iliya97pro@yandex.ru>
2022-05-14 16:35:22 +0300

    add CarService and fix CarDao

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
@@ -4,10 +4,12 @@ import hiber.model.Car;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+@Repository
 public class CarDaoImp implements CarDao{
 
     @Autowired
