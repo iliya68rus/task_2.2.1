ab0330c344c75b8f59022b721e4e21da93e3f170
iliya68rus <iliya97pro@yandex.ru>
2022-05-16 11:04:50 +0300

    сделал запрос через TypedQuery

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
@@ -1,15 +1,16 @@
 package hiber.dao;
 
 import hiber.model.Car;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
+@Transactional
 public class CarDaoImp implements CarDao {
 
     @Autowired
@@ -29,20 +30,11 @@ public class CarDaoImp implements CarDao {
 
     @Override
     public Car findCarByModelAndNumber(String model, int series) {
-        Car car = null;
-        try {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            List<Car> carList = session
-                    .createQuery("from Car car where car.model = :model and car.series = :series")
-                    .setParameter("model", model)
-                    .setParameter("series", series)
-                    .getResultList();
-            session.getTransaction().commit();
-            car = carList.get(0);
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-        return car;
+        TypedQuery<Car> query = sessionFactory
+                .getCurrentSession()
+                .createQuery("from Car car where car.model = :model and car.series = :series")
+                .setParameter("model", model)
+                .setParameter("series", series);
+        return query.getSingleResult();
     }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,15 +2,16 @@ package hiber.dao;
 
 import hiber.model.Car;
 import hiber.model.User;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
+@Transactional
 public class UserDaoImp implements UserDao {
 
     @Autowired
@@ -30,19 +31,25 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public User findUserByCar(Car car) {
-        User user = null;
-        try {
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-            List<User> userList = session
-                    .createQuery("from User user where user.car = :car")
-                    .setParameter("car", car)
-                    .getResultList();
-            session.getTransaction().commit();
-            user = userList.get(0);
-        } catch (RuntimeException e) {
-        }
-        return user;
+//        User user = null;
+//        try {
+//            Session session = sessionFactory.openSession();
+//            session.beginTransaction();
+//            List<User> userList = session
+//                    .createQuery("from User user where user.car = :car")
+//                    .setParameter("car", car)
+//                    .getResultList();
+//            session.getTransaction().commit();
+//            user = userList.get(0);
+//        } catch (RuntimeException e) {
+//        }
+//        return user;
+
+        TypedQuery<User> query = sessionFactory
+                .getCurrentSession()
+                .createQuery("from User user where user.car = :car")
+                .setParameter("car", car);
+        return query.getSingleResult();
     }
 
 }
